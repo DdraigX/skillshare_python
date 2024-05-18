70cefa5f08980f8f9a8ed8807ea1879e22b43f2d
DdraigX <6390496+DdraigX@users.noreply.github.com>
2024-05-17 20:55:51 -0400

    Initial Commit

---
+++ notebook.py
@@ -0,0 +1,48 @@
+# print("Hello there!")
+
+# kingname = input("Please type your name in now: ")
+
+# print("Hello King " + kingname)
+
+# Jewelnumb = input("Number of Jewels: ")
+
+# JewelVal = input("Average value of each Jewel: ")
+
+# JewelTotal = int(Jewelnumb) * int(JewelVal)
+# print(str(JewelTotal))
+
+
+# musketNames = ["Athos","Pothos","Aramis"]
+# musketAge = ["55","34","67"]
+
+# musketNames.insert(0,"Darty")
+# musketAge.append("16")
+
+# print(musketNames)
+# print(musketAge)
+
+# tempName = musketNames.pop(0)
+# musketNames.append (tempName)
+
+# print (musketNames)
+# print  (musketAge)
+# print (str(len(musketNames)))
+
+# #index of arraylist
+# whotokill = input("Hello King" + kingname + "Please pick the scapegoat: ")
+
+# #prints names in specific array location
+# print(musketNames[int(whotokill)-1])
+# print(musketAge[int(whotokill)- 1])
+
+# print("Name: ", musketNames[int(whotokill)-1] + " " + "Age: ", musketAge[int(whotokill)- 1])
+
+# #deletes entry in array/list 
+# del musketNames[int(whotokill)-1]
+# del musketAge[int(whotokill)- 1]
+
+# print(musketNames)
+# print(musketAge)
+
+
+
